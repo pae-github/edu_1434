209b1f416a7b9ab83acd20cd4c85190cb5cdf9b4
pae-github <73995659+pae-github@users.noreply.github.com>
2020-11-09 00:43:29 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,8 +1,13 @@
+package com.company;
 /* Объяви переменные a, b типа int и переменную s типа String.
 Сразу же в строке объявления присвой им любые разные значения. */
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a = 5;
+        int b = 10;
+        String s = "Sum";
+     //   int sum = a+b;
+     //   System.out.println("Sum: "+sum);
     }
 }
