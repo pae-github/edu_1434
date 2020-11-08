8e025c4dba79bb9b2c70bea5947724b64213e5f9
pae-github <73995659+pae-github@users.noreply.github.com>
2020-11-09 01:22:52 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,9 +1,12 @@
+package com.company;
 /* Дана переменная number.
 Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
 
 public class Main {
     public static void main(String[] args) {
         int number = 25;
-        //напишите тут ваш код
+        //   System.out.println(number*number);
+        int square = number*number;
+        System.out.println(square);
     }
 }
