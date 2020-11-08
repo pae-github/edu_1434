d1dcf96e40b5c464d23e2dd76d0bff67eed22833
pae-github <73995659+pae-github@users.noreply.github.com>
2020-11-09 00:22:08 +0300

    Update Main.java

--- task_1.2/Main.java
+++ task_1.2/Main.java
@@ -1,3 +1,5 @@
+package com.company;
+
 /*
 Закомментируй ненужные строки кода, чтобы на экран вывелась надпись:
 2 плюс 3 равно 5
@@ -10,15 +12,15 @@ public class Main {
         int a = 3;
         int b = 2;
 
-        System.out.print("два");
+      //  System.out.print("два");
         System.out.print(b);
         System.out.print(" плюс ");
-        System.out.print(" минус ");
+      //  System.out.print(" минус ");
         System.out.print(a);
-        System.out.print("три");
+      //  System.out.print("три");
         System.out.print(" равно ");
-        System.out.print(" будет ");
-        System.out.print("пять");
+      //  System.out.print(" будет ");
+     //   System.out.print("пять");
         System.out.print(a + b);
     }
 }
