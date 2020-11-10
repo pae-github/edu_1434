8d9151340952705ea2924ba8744ee8a007cb0381
pae-github <73995659+pae-github@users.noreply.github.com>
2020-11-10 12:58:17 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,9 +1,18 @@
+package com.company;
 /*
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
 
+import java.util.Scanner; //импорт для ввод с клавиатуры
+
 public class Main {
     public static void main(String[] args) {
-        // Ваш код
+        Scanner input = new Scanner(System.in);
+        // input - ввод информации
+        // new Scanner(System.in); ввод с клавиатуры
+        System.out.print("Введите число: ");
+        double Number = input.nextDouble(); // nextDouble () для дробных чисел
+        double Result = Number + (Number * (15.0/100));
+        System.out.print(Number+" + 15% = "+Result);
     }
 }
